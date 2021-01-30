01b57baac375e97bf8928a120dcbb2e899d0ec94
Adam Wardell <adamwardell@gmail.com>
2021-01-30 12:22:36 -0800

    ragdolling

---
+++ Loopowl Challenge/Assets/Scripts/RagdollActivator.cs
@@ -0,0 +1,53 @@
+﻿
+using UnityEngine;
+
+public class RagdollActivator : MonoBehaviour
+{
+    //Isn't it annoying how behaviours and colliders don't have a common parent
+    //or interface with .enabled?
+    [SerializeField]
+    public Behaviour[] behavioursToActivate;
+
+    [SerializeField]
+    public Behaviour[] behavioursToDeactivate;
+
+    [SerializeField]
+    public Collider[] collidersToActivate;
+
+    [SerializeField]
+    public Collider[] collidersToDeactivate;
+
+    [SerializeField]
+    public Rigidbody[] rigidbodiesToActivate;
+
+	//[SerializeField]
+	//public Rigidbody gameplayRigidbody; //needed?
+
+	public void Activate()
+	{
+        foreach (var behaviour in behavioursToActivate)
+		{
+            behaviour.enabled = true;
+		}
+
+        foreach (var behaviour in behavioursToDeactivate)
+		{
+            behaviour.enabled = false;
+		}
+
+        foreach (var col in collidersToActivate)
+		{
+            col.enabled = true;
+		}
+
+        foreach (var col in collidersToDeactivate)
+		{
+            col.enabled = false;
+		}
+
+        foreach (var rig in rigidbodiesToActivate)
+		{
+            rig.isKinematic = false;
+		}
+	}
+}
